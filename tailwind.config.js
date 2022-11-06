c562a537c955a9424bba76008bfb4013a4821d01
GOGA08 <91950897+GOGA08@users.noreply.github.com>
2022-11-07 02:57:26 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,php}"],
+  theme: {
+  
+    extend: {
+      fontFamily: {
+        'Poire': ['Poiret One'],
+      }
+    
+    },
+  },
+  plugins: [],
+}
